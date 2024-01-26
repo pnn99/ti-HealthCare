0e4ff7a4a1d9d635df9dd1bb2a869108d27d39f1
Gabriel Magalhães <138159458+Gabriel-Magalhaes1401@users.noreply.github.com>
2024-01-26 19:11:10 -0300

    Páginas de Login e Cadastro quase prontas.

---
+++ TI/back-end/model/login.js
@@ -0,0 +1,8 @@
+function login() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    // Adicionar lógica pra processar o login, validar os dados, etc.
+
+    console.log('Login realizado com sucesso!');
+}
